d2027c5bf028e56f425a6135721db8ecc3f1fc14
Mayken <mayu-satu@hotmail.com>
2023-06-18 00:27:16 -0500

    LISTA DE SERVICIOS MST

--- src/Servi.js
+++ src/Servi.js
@@ -2,7 +2,7 @@
     {
         "id":1,
         "tipo": "Cambio de aceite",
-        "costo":  100
+        "costo":  30
     },
     {
         "id":2,
@@ -12,57 +12,57 @@
     {
         "id":3,
         "tipo": "Alineación y balanceo",
-        "costo":  100
+        "costo":  35
     },
     {
         "id":4,
         "tipo": "Diagnóstico general",
-        "costo":  100
+        "costo":  50
     },
     {
         "id":5,
         "tipo": "Revisión sistema eléctrico",
-        "costo":  100
+        "costo":  57.43
     },
     {
         "id":6,
         "tipo": "Revisión de la suspensión",
-        "costo":  100
+        "costo":  60
     },
     {
         "id":7,
         "tipo": "Pintura automotriz",
-        "costo":  100
+        "costo":  800
     },
     {
         "id":8,
         "tipo": "Venta y montaje de llantas",
-        "costo":  100
+        "costo":  400
     },
     {
         "id":9,
         "tipo": "Reparación del motor",
-        "costo":  100
+        "costo":  2000
     },
     {
         "id":10,
         "tipo": "Reparación de sistemas de refrigeración",
-        "costo":  100
+        "costo":  300
     },
     {
         "id":11,
         "tipo": "Cambio de bujías",
-        "costo":  100
+        "costo":  70
     },
     {
         "id":12,
         "tipo": "Cambio de batería",
-        "costo":  100
+        "costo":  175
     },
     {
         "id":13,
         "tipo": "Reparación y recarga de sistemas de aire acondicionado",
-        "costo":  100
+        "costo":  254.53
     }
 ]
 
